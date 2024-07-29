1cd89ccc56e035f4bc1b27a7077f206daef99402
DestMarco <marcolino1424@gmail.com>
2024-07-29 12:15:52 +0200

    ++

--- JavaScript/corso.js
+++ JavaScript/corso.js
@@ -8,9 +8,10 @@ Esercizo di base che stampa a schermo la stringa di testo "ciao".
 La funzione console.log, prende come input una stringa (nell'esempio
 la stringa "ciao" e produce una stampa a schermo della stessa.)
  */
-
+ /*
 console.log("ciao");
 process.exit(0);
+*/
 
 
 /*
@@ -21,12 +22,10 @@ npm install prompt-sync
 */
 
 const prompt = require('prompt-sync')();
-
-/*
 const str = prompt('Enter a string: ');
 console.log("Hai inserito " + str);
 process.exit(0);
-*/
+
 
 /*
 var a;
